bbb2ff6ddc715827904dda0285908de4c811a7f6
paulo <paulojrzti@gmail.com>
2025-07-11 12:20:12 -0300

    feat/avatars-list-components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,34 @@
-import { Header, CardComponent } from '@/components'
+import { AvatarList, Header, CardComponent } from '@/components'
 import { Container } from '@mui/material'
+import { currencyConverter } from '@/utils'
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: 'dnc-avatar.svg',
+      name: 'Nome Sobrenome',
+      subtitle: currencyConverter(1223.54),
+    },
+    {
+      avatar: 'dnc-avatar.svg',
+      name: 'Nome Sobrenome',
+      subtitle: currencyConverter(1223.54),
+    },
+    {
+      avatar: 'dnc-avatar.svg',
+      name: 'Nome Sobrenome',
+      subtitle: currencyConverter(1223.54),
+    },
+  ]
   return (
     <>
       <Header></Header>
       <Container maxWidth="lg">
-        <CardComponent className="success">Card</CardComponent>
+        <CardComponent>Card</CardComponent>
+        <CardComponent>
+          <AvatarList listData={mockListData} />
+        </CardComponent>
       </Container>
-      <h1>home</h1>
     </>
   )
 }
